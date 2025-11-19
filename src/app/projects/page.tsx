4b4973f2364a2bea534b0aebd6035b74e4ad29c4
Ethan Sie <esie50@stuy.edu>
2025-11-19 13:34:52 -0800

    project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,6 +3,7 @@ export default function ProjectsPage() {
     <div>
       <h1 className="text-3xl font-bold mb-6">Projects</h1>
       <p className="text-gray-700 mb-8">A list of projects will go here.</p>
+      <iframe src="test.html" width="320" height="340"></iframe>
     </div>
   );
 }
