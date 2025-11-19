ae4a00144781a6a32827b98b3f7e15dd6663f382
Ethan Sie <esie50@stuy.edu>
2025-11-19 14:11:43 -0800

    please run

---
+++ src/app/competitions/mmc2025/page.tsx
@@ -0,0 +1,11 @@
+export default function CircuitBuilder() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="/circuitBuilder.html"
+        className="w-full h-full border-0"
+        title="Circuit Builder"
+      />
+    </div>
+  );
+}
